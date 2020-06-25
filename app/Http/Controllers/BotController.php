aeae5715b775461243902b84b16a474196cab4d2
tabiojisann <takumiezumi@icloud.com>
2020-06-25 21:59:20 +0900

    create SERCH BOT

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -2,12 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\Gurunavi;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use LINE\LINEBot;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 
 class BotController extends Controller
 {
     public function index()
     {
         return view('bot.index');
+    }
+
+    public function restaurants(Request $request) 
+    {
+        Log::debug($request->header());
+        Log::debug($request->input());
+
+        $httpClient = new CurlHTTPClient(env('LINE_ACCESS_TOKEN'));
+        $lineBot = new LINEBot($httpClient, ['channelSecret' => env('LINE_CHANNEL_SECRET')]);
+
+        $signature = $request->header('x-line-signature');
+        if (!$lineBot->validateSignature($request->getContent(), $signature)) {
+            abort(400, 'Invalid signature');
+        }
+
+        $events = $lineBot->parseEventRequest($request->getContent(), $signature);
+
+        Log::debug($events);
+
+        foreach ($events as $event) {
+            if (!($event instanceof TextMessage)) {
+                Log::debug('Non text message has come');
+                continue;
+            }
+
+            $gurunavi = new Gurunavi();
+            $gurunaviResponse = $gurunavi->searchRestaurants($event->getText());
+
+            if (array_key_exists('error', $gurunaviResponse)) {
+                $replyText = $gurunaviResponse['error'][0]['message'];
+                $replyToken = $event->getReplyToken();
+                $lineBot->replyText($replyToken, $replyText);
+                continue;
+            }
+
+            $replyText = '';
+            foreach($gurunaviResponse['rest'] as $restaurant) {
+                $replyText .=
+                    $restaurant['name'] . "\n" .
+                    $restaurant['url'] . "\n" .
+                    "\n";
+            }
+
+            $replyToken = $event->getReplyToken();
+            $lineBot->replyText($replyToken, $replyText);
+        }
     }
 }
